4d8acb6e91c999bde0651af435f5b0e32912f95e
Anne <suelenornellas@gmail.com>
2023-11-20 16:16:49 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){ //JQuery funciona a partir do cifrão
+    //alert("Ola jQuery")
+    console.log(document.querySelector('header button'));
+    console.log($('header button'));
+})
